32cefffdd650795a33ab511a0cef40a56074a6c9
Nylilsa <nylilsa2468@gmail.com>
2025-09-15 17:59:53 +0200

    [Tags] Finished creating autorun script

--- scripts/node/create-categories-mapping.js
+++ scripts/node/create-categories-mapping.js
@@ -1,23 +1,79 @@
 const fs = require('fs');
+// const { format } = require('fracturedjsonjs');
+const { Formatter, EolStyle, FracturedJsonOptions } = require('fracturedjsonjs');
 
-function init() {
-    const treeObj = JSON.parse(fs.readFileSync("json/glitch-tree.json"));
-    const categoriesObj = JSON.parse(fs.readFileSync("json/glitch-tree.json"));
-    const thGames = Object.keys(treeObj);
+const TREE_OBJ = JSON.parse(fs.readFileSync("json/glitch-tree.json"));
+const CATEGORIES_OBJ = JSON.parse(fs.readFileSync("json/categories.json"));
+
+function cleanCategoriesObj() {
+    const indicesTags = Object.keys(CATEGORIES_OBJ["tags"]);
+    for (let i = 0; i < indicesTags.length; i++) {
+        const index = indicesTags[i];
+        delete CATEGORIES_OBJ["tags"][index]["tree-mapping"];
+    }
+    delete CATEGORIES_OBJ["related"];
+    CATEGORIES_OBJ["related"] = {};
+}
+
+function mapping() {
+    const thGames = Object.keys(TREE_OBJ);
     for (let i = 0; i < thGames.length; i++) {
         const game = thGames[i];
-        const pageIndex = Object.keys(treeObj[game]);
+        const pageIndex = Object.keys(TREE_OBJ[game]);
         for (let j = 0; j < pageIndex.length; j++) {
-            const tagList = treeObj[game][j]?.categories?.tags;
-            const related = treeObj[game][j]?.categories?.related;
-            if (!tagList) continue;
-            console.log(game +" "+ j);
-            console.log(tagList);
+            const tagList = TREE_OBJ[game][j]?.categories?.tags;
+            const related = TREE_OBJ[game][j]?.categories?.related;
+            if (tagList) {
+                addToCategories(game, j, tagList);
+            }
+            if (related) {
+                addToRelated(game, j, related);
+            }
         }
 
     }
+}
 
+function addToCategories(game, pageIndex, tagList) {
+    for (let i = 0; i < tagList.length; i++) {
+        const tagIndex = tagList[i];
+        const tagObject = CATEGORIES_OBJ["tags"][tagIndex];
+        if (tagObject === undefined) {
+            throw new Error(`Game ${game} page ${pageIndex} index ${tagIndex} is an invalid index.`);
+        }
+        tagObject["tree-mapping"] = tagObject["tree-mapping"] ?? {};
+        tagObject["tree-mapping"][game] = tagObject["tree-mapping"][game] ?? [];
+        tagObject["tree-mapping"][game].push(pageIndex);
+    }
+}
 
+function addToRelated(game, pageIndex, relatedId) {
+    const relatedObject = CATEGORIES_OBJ["related"];
+    relatedObject[relatedId] = relatedObject[relatedId] ?? {};
+    relatedObject[relatedId][game] = relatedObject[relatedId][game] ?? [];
+    relatedObject[relatedId][game].push(pageIndex);
+}
+
+function saveJsonFiles() {
+    const options = new FracturedJsonOptions();
+    options.MaxInlineComplexity = 5;
+    options.MaxTotalLineLength = 200;
+    options.MinCompactArrayRowItems = 1;
+    options.AlwaysExpandDepth = 0;
+
+    const formatter = new Formatter();
+    formatter.Options = options;
+
+    const formattedJson = formatter.Reformat(JSON.stringify(CATEGORIES_OBJ));
+
+    fs.writeFileSync("json/categories.json", formattedJson);
+    console.log("Successfully wrote to json/categories.json")
+}
+
+function init() {
+    cleanCategoriesObj();
+    mapping();
+    saveJsonFiles();
 }
 
 init();
