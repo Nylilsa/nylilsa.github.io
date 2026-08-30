bd15d890b192a95a77929f2a243c4e1a06db09d5
Nylilsa <nylilsa2468@gmail.com>
2026-08-30 06:19:27 +0200

    [SEO, th20-6, th20-17, th13-5] Fixed sitemap.xml, moved some pages around for indexing purposes

--- scripts/node/create-sitemap.js
+++ scripts/node/create-sitemap.js
@@ -1,50 +1,49 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const BASE_URL = "https://nylilsa.github.io";
-const MAPPING_FILE = path.join(__dirname, '../../json/glitch-tree.json'); 
-const OUTPUT_SITEMAP = path.join(__dirname, '../../sitemap.xml');
+const DIST_DIR = path.join(__dirname, "../../dist");
+const OUTPUT_SITEMAP = path.join(__dirname, "../../sitemap.xml");
 
 function generateSitemap() {
-    try {
-        if (!fs.existsSync(MAPPING_FILE)) {
-            console.error(`Mapping file not found at: ${MAPPING_FILE}`);
-            return;
-        }
-        const rawData = fs.readFileSync(MAPPING_FILE, 'utf8');
-        const mapping = JSON.parse(rawData);
-        let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-        xml += '<urlset xmlns="http://sitemaps.org">\n';
-        // hardcoded home + wr pages
-        xml += `    <url><loc>${BASE_URL}/</loc></url>\n`;
-        xml += `    <url><loc>${BASE_URL}/wr</loc></url>\n`;
-        for (const gameKey in mapping) {
-            const gameEntries = mapping[gameKey];
-            if (typeof gameEntries === 'object' && gameEntries !== null) {
-                for (const entryId in gameEntries) {
-                    const info = gameEntries[entryId];
-                    if (info && info["url-name"]) {
-                        const names = Array.isArray(info["url-name"]) ? info["url-name"] : [info["url-name"]];
-                        names.forEach(name => {
-                            const cleanName = name.replace(/^\//, '');
-                            xml += `    <url><loc>${BASE_URL}/bugs/${gameKey}/${cleanName}</loc></url>\n`;
-                        });
-                    }
+    const urls = [];
+
+    function walk(dir) {
+        for (const entry of fs.readdirSync(dir, { withFileTypes: true })) {
+            const fullPath = path.join(dir, entry.name);
+
+            if (entry.isDirectory()) {
+                walk(fullPath);
+            } else if (entry.name.endsWith(".html") && entry.name !== "404.html") {
+                const relative = path.relative(DIST_DIR, fullPath);
+                let urlPath = relative.replace(/\\/g, "/");
+
+                if (urlPath === "index.html") {
+                    // home directory, base
+                    urlPath = "";
+                } else if (urlPath.endsWith("/index.html")) {
+                    urlPath = urlPath.slice(0, -"/index.html".length);
+                } else {
+                    urlPath = urlPath.replace(/\.html$/, "");
                 }
+                urls.push(`/${urlPath}`);
             }
         }
-        xml += '</urlset>\n';
-        fs.writeFileSync(OUTPUT_SITEMAP, xml, 'utf8');
-        console.log(`sitemap.xml generated with all nested game URLs`);
-        
-    } catch (err) {
-        console.error('Sitemap Error:', err);
     }
+
+    walk(DIST_DIR);
+
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+
+    for (const url of urls) {
+        xml += `    <url><loc>${BASE_URL}${url}</loc></url>\n`;
+    }
+
+    xml += "</urlset>\n";
+
+    fs.writeFileSync(OUTPUT_SITEMAP, xml, "utf8");
+    console.log(`Generated sitemap with ${urls.length} URLs`);
 }
 
-/* 
-todo: make it better
-only bug pages are added now but also add guides and stuff later
-also bug: when visiting https://nylilsa.github.io/wr directly, does not work (libraries are not loaded properly)
-*/
 generateSitemap();
